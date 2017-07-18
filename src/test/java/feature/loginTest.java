55d068536292fe67e1e26580decb0f108777e63d
Alon zanbar <alon.zanbar@hp.com>
2017-07-18 07:06:39 -0700

    fix feature

--- src/test/java/feature/loginTest.java
+++ src/test/java/feature/loginTest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(OctaneCucumber.class)
 @CucumberOptions(plugin={"junit:junitResult.xml"},
-        features= "src/test/resources/feature/login_2-2125.feature_back")
+        features= "src/test/resources/feature/login_2-2125.feature")
 public class loginTest{
 
 }
